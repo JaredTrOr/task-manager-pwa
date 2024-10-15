5bcf054c49419cc0280a4b1320ccb5cc3594b2e9
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-15 00:51:59 -0600

    Creación de consumo de servicio de auth en páginas de login y signup, validación de inputs, registro e inicio de sesión de usuarios

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  formGroup!: FormGroup;
+  errorMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+  
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    this.authService.login(this.formGroup.value).subscribe({
+      next: response => {
+        if (!response.success) {
+
+          if (response.message.includes('error')) {
+            this.errorMessage = 'Hubo un error al realizar el registro, por favor intentar denuevo';
+            console.log(response.message);
+            return;
+          }
+
+          this.errorMessage = response.message;
+        }
+        else {
+          this.authService.setToken(response.data?.token!);
+          // Establish user data ->
+          this.router.navigate(['/home']);
+          this.formGroup.reset();
+        }
+      },
+      error: err => {
+        this.errorMessage = 'Hubo un error al realizar el registro, por favor intentar denuevo';
+        console.log(err);
+      } 
+    });
+  }
 }
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
 
+  formGroup!: FormGroup;
+  isLoading: boolean = false;
+
+  // Message error general
+  errorMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['',[Validators.required, Validators.pattern('^[a-zA-Z0-9_.-]+$')]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.authService.signup(this.formGroup.value)
+    .subscribe({
+      next: response => {
+        if (!response.success) {
+          if (response.message.includes('error')) {
+            this.errorMessage = 'Hubo un error al realizar el registro, por favor intentar denuevo';
+            console.log(response.message);
+            return;
+          }
+
+          this.errorMessage = response.message;
+        }
+        else {
+          this.authService.setToken(response.data?.token!);
+          // Establish user data ->
+          this.router.navigate(['/home']);
+          this.formGroup.reset();
+        }
+      },
+      error: err => {
+        this.errorMessage = 'Hubo un error al realizar el registro, por favor intentar denuevo';
+        console.log(err);
+      }
+    });
+
+    this.isLoading = false;
+  }
 }
