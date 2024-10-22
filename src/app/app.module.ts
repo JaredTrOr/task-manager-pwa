d9ab7925a11c0e3bda7d739b37548182b1a82676
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-21 23:30:40 -0600

    Creación del interceptador de token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 import { ModalListComponent } from './components/modal-list/modal-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LoadingComponent } from './components/loading/loading.component';
+import { CreateTaskComponent } from './pages/create-task/create-task.component';
+import { TokenInterceptorService } from './services/token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -22,12 +24,14 @@ import { LoadingComponent } from './components/loading/loading.component';
     SidebarComponent,
     HomeComponent,
     ModalListComponent,
-    LoadingComponent
+    LoadingComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     PickerComponent,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
@@ -37,7 +41,13 @@ import { LoadingComponent } from './components/loading/loading.component';
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService, 
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
