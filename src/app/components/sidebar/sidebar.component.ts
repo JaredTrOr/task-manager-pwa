fe41cdada8c8c2c69cfa5f3ac3acb366bf3582e3
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-22 19:21:15 -0600

    Logica para borrar listas y logout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Input, NgZone, OnInit, Output } from '@angular/core';
 import { ListTypeService } from '../../services/list-type.service';
 import ListType from '../../models/list.interface';
+import { User } from '../../models/user.interface';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,18 +14,55 @@ export class SidebarComponent implements OnInit {
   @Input() isSidebarOpen!:boolean;
   @Input() isModalOpen!: boolean;
   @Output() openModalEvent = new EventEmitter<boolean>();
+
+  // Http info
   listTypeArray: ListType[] = [];
+  userInfo?: User;
 
   constructor(
-    private listTypeService: ListTypeService
-  ) {}
+    public listTypeService: ListTypeService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
-    this.listTypeArray = this.listTypeService.getListType();
+    this.listTypeService.getListsTypes().subscribe({
+      next: response => {
+        if (response.data) {
+          this.listTypeService.setListTypeArray(response.data);
+        }
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
   }
 
   openModal() {
     this.isModalOpen = !this.isModalOpen;
     this.openModalEvent.emit(this.isSidebarOpen);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  deleteList(index: number) {
+    // [element1, element2] [0]
+    const element = this.listTypeService.getListTypeArray()[index];
+    const listId = element._id;
+
+    //Borro del arreglo
+    this.listTypeService.removeListType(index);
+
+    //Borrar de la BD
+    this.listTypeService.deleteListType(listId!).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+    
   }
 }
