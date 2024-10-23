14fcc24c2cf925db6b7eaa5c5a413a50b94ecc06
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-23 16:49:20 -0600

    Obtener datos directamente desde el home y apertura y cierre de modal de create-task

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,57 @@
-import { Component, HostListener, NgZone } from '@angular/core';
-import TaskToDo from '../../models/task.interface';
-import ListType from '../../models/list.interface';
+import { Component, HostListener, NgZone, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { ListTypeService } from '../../services/list-type.service';
+import { TaskService } from '../../services/task.service';
+import { User } from '../../models/user.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  // Component events
   isSidebarOpen = true;
   isResponsive = false;
   isModalOpen = false;
+  isCreateTaskOpen = false;
 
-  taskList: TaskToDo[] = [];
-  listsTypes: ListType[] = [];
+  userInfo?: User;
 
-  constructor() {
+
+  constructor(
+    private taskService: TaskService,
+    public listTypesService: ListTypeService
+  ) {
     this.checkScreenSize();
+  }
+
+  ngOnInit(): void {
+    // Get tasks
+    this.taskService.getTasks().subscribe({
+      next: response => {
+        if (response.data) {
+          this.taskService.setTaskArray(response.data);
+        }
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+
+    // Get list types
+    this.listTypesService.getListsTypes().subscribe({
+      next: response => {
+        if (response.data) {
+          this.listTypesService.setListTypeArray(response.data);
+        }
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+
   }
 
   @HostListener('window:resize', ['$event'])
@@ -47,5 +81,13 @@ export class HomeComponent {
 
   closeModal(toggledValue : boolean) {
     this.isModalOpen = toggledValue;
+  }
+
+  openCreateTask() {
+    this.isCreateTaskOpen = true;
+  }
+
+  closeCreateTask(toggledValue : boolean) {
+    this.isCreateTaskOpen = toggledValue;
   }
 }
