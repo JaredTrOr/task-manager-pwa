164b770194751c37163a523a9d7e81fa59bb87ae
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-24 16:23:52 -0600

    creación de util para convertir date type a string type

---
+++ src/app/utils/date-converter.ts
@@ -0,0 +1,9 @@
+export function convertDateToString (date: string) {
+
+    const dateObj = new Date(date);
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1;
+    const year = dateObj.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
