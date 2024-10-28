150e019688e9e047864e602141e2c736962bb693
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-27 22:43:11 -0600

    Agregación de método para actualizar user info

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,13 +9,26 @@ import { User } from '../models/user.interface';
 export class UserService {
 
   private URL: string = 'http://localhost:3000/user';
+  userInfo!: User;
 
   constructor(
     private http: HttpClient
   ) { }
 
+  setUserInfo(user: User) {
+    this.userInfo = user;
+  }
+
+  getUserInfoProvider(): User {
+    return this.userInfo;
+  }
+
   // Http requests
   getUserInfo() {
     return this.http.get<ApiResponse<User>>(`${this.URL}/get-profile`);
+  }
+
+  udpateUserInfo(user: User) {
+    return this.http.put<ApiResponse<User>>(`${this.URL}/update-profile`, user);
   }
 }
