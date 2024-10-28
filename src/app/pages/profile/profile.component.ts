ddda74afa734119bb142fa10e55b51fa6807edda
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-27 22:41:56 -0600

    Agregación de mensajes de error en editar perrfil, y cambio de foto de perfil para subirla a firebase

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user.interface';
 import { UserService } from '../../services/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-profile',
@@ -10,20 +11,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ProfileComponent implements OnInit {
 
-  userInfo!: User;
   formGroup!: FormGroup;
+  selectedImage!: any;
 
   constructor(
-    private userService: UserService,
-    private formBuilder: FormBuilder
+    public userService: UserService,
+    private formBuilder: FormBuilder,
+    private fireStorage: AngularFireStorage
   ) { }
 
   ngOnInit(): void {
+    this.setupForm();
+
     this.userService.getUserInfo().subscribe({
       next: response => {
         if (response.success) {
-          this.userInfo = response.data!;
-          console.log(this.userInfo)
+          this.userService.setUserInfo(response.data!);
+          this.formGroup.patchValue({
+            name: response.data?.name,
+            email: response.data?.email,
+            username: response.data?.username
+          })
         }
       },
       error: err => {
@@ -31,7 +39,6 @@ export class ProfileComponent implements OnInit {
       }
     });
 
-    this.setupForm();
   }
 
   setupForm() {
@@ -42,8 +49,52 @@ export class ProfileComponent implements OnInit {
     });
   }
 
-  editProfile() {
+  changeProfilePhoto(e: any) {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      this.selectedImage = file;
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.userService.setUserInfo({
+          ...this.userService.getUserInfoProvider(),
+          image: reader.result as string
+        });
+      }
+    }
+  }
+
+  async editProfile() {
+    if (!this.formGroup.valid) {
+      this.formGroup.markAllAsTouched();
+      return;
+    }
     
+    // Upload image to firebase
+    if (this.selectedImage) {
+      const filePath = `profile-images/${this.selectedImage.name}`;
+      const uploadTask = await this.fireStorage.upload(filePath, this.selectedImage);
+      const url = await uploadTask.ref.getDownloadURL();
+
+      // Preview image
+      this.userService.setUserInfo({
+        ...this.userService.getUserInfoProvider(),
+        image: url
+      });
+    }
+
+    this.formGroup.value.image = this.userService.getUserInfoProvider().image;
+    this.userService.setUserInfo(this.formGroup.value);
+    this.userService.udpateUserInfo(this.formGroup.value).subscribe({
+      next: response => {
+        if (response.success) {
+          console.log(response);
+        }
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
   }
 
 }
